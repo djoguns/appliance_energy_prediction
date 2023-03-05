36bb75df2b32e0daa77f9ea98cf0afcd207683bc
djoguns <dawofe@gmail.com>
2023-03-05 15:32:55 +0000

    model building added

--- data_ingestion/ingest.py
+++ data_ingestion/ingest.py
@@ -1,4 +1,13 @@
 import pandas as pd
-from parameters.parameters import file_path
 
-appliance_energy_data = pd.read_csv(file_path)
+def get_data(f):
+    """
+    This is a function that takes a file path and read in data
+    with pandas
+    : f: filepath
+    : df: resulting data
+    """
+    
+    df = pd.read_csv(f)
+
+    return df
